c75b292e0e684a032c6e454eb228975aae931483
Ilya Grebtsov <Ilya.Grebtsov@dataart.com>
2018-06-24 01:00:03 +0300

    style improvements, animation; signature on the welcome screen.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,13 +14,16 @@ class Welcome extends Component {
     render() {
         return (
             <div className="Welcome">
-                <h1>Battle ships</h1>
-                <p>There is a square 10 by 10 cells battleground which hides one L shaped, one I shaped and two dot shaped battle ships.
-                   Initial they cannot overlap and must not touch one another so there is at least a single cell between them.
-                   Any battle ship rotation and position are random.
-                   Good luck in the search!
-                </p>
-                <button className="btn btn-default" onClick={this.props.onStart}>Start game</button>
+                <div className="content">
+                    <h1>Battle ships</h1>
+                    <p>There is a square 10 by 10 cells battleground which hides one L shaped, one I shaped and two dot shaped battle ships.
+                       Initial they cannot overlap and must not touch one another so there is at least a single cell between them.
+                       Any battle ship rotation and position are random.
+                       Good luck in the search!
+                    </p>
+                    <button className="btn btn-default" onClick={this.props.onStart}>Start game</button>
+                    <div className="signature">Developed by Ilya Grebtsov</div>
+                </div>
             </div>
         );
     }
