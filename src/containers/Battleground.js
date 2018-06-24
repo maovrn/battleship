e490eb19077e1f84b10511bd2d04c8f355a633a1
Ilya Grebtsov <Ilya.Grebtsov@dataart.com>
2018-06-24 17:42:47 +0300

    improve code style in battleground render method

--- src/containers/Battleground.js
+++ src/containers/Battleground.js
@@ -31,6 +31,7 @@ class Battleground extends Component {
 
     click = (e) => {
         e.stopPropagation();
+        // throw SHOT action if user clicks non-checked cell
         let x = Number.parseInt(e.target.getAttribute('data-x'), 10),
             y = Number.parseInt(e.target.getAttribute('data-y'), 10);
         if (Number.isInteger(x) && Number.isInteger(y)) {
@@ -41,44 +42,49 @@ class Battleground extends Component {
         }
     }
 
-    render() {
-
-        var scale = 50;
-
-        // draw cells
-        var cells = [];
+    renderCells = (scale) => {
+        let ret = [];
         for (let i=0; i<maxY; i++) {
             for (let j=0; j<maxX; j++) {
-                let point = this.props.matrix[j][i] || 0;
+                let point = this.props.matrix[j][i];
                 let status = (point === sign.hit ? 'hit' : (point === sign.miss ? 'miss' : ''));
-                cells.push(
+                ret.push(
                     <rect key={'cell_' + i + '_' + j} className={'cell ' + status} x={i * scale} y={j * scale} data-x={i} data-y={j}/>
                 );
             }
         }
+        return ret;
+    }
 
-        // draw fired ships
-        var shipFrames = [];
+    renderShips = (scale) => {
+        let ret = [];
         this.props.ships.forEach(ship => {
-            if (ship.frame && (ship.decks === ship.hits)) {
-                let pointsScaled = ship.frame.reduce((acc, point) => {
+            // draw only the ships that are fired
+            if (ship.decks === ship.hits) {
+                let points = ship.frame.reduce((acc, point) => {
                     let [x, y] = point;
                     x = (ship.x + x) * scale;
                     y = (ship.y + y) * scale;
                     return acc + ' ' + [x, y].join();
                 }, "");
-                shipFrames.push(
-                    <polygon key={'ship_'+ship.x+'_'+ship.y} className="ship-frame" points={pointsScaled}/>
+
+                ret.push(
+                    <polygon key={'ship_'+ship.x+'_'+ship.y} className="ship-frame" points={points}/>
                 );
             }
         });
+        return ret;
+    }
+
+    render() {
+        const scale = 50; // magic value according CSS styles
 
         return (
             <div className="Battleground">
                 <BattlegroundBack cols={maxX} rows={maxY}>
                     <svg className="grid" onClick={this.click}>
-                        {cells}
-                        {shipFrames}
+                        {this.renderCells(scale)}
+                        {this.renderShips(scale)}
                     </svg>
                 </BattlegroundBack>
             </div>
